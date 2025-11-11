658b2a7aeacd65563aa94b09f0e5562b46f875d6
SrinivasAttiliLTTS <srinivasu.attili@ltts.com>
2025-11-11 12:04:15 +0530

    fix: homepage path for Render static site

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
 // Update this after backend is deployed on Render
-export const API_URL = "https://ai-resume-backend.onrender.com";
+export const API_URL1 = "https://ai-resume-backend.onrender.com";
+export const API_URL = "https://ai-resume-screen-backend.onrender.com";
